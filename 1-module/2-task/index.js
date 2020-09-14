81f2b5f17b18bd7dfc7105271e9844979ea14086
Alexander Bezverhuk <albezver@gmail.com>
2020-09-14 13:33:28 +0300

    Added boolean check for inserted argument at prompt

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,15 +10,24 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
+/**
+ * Эту функцию трогать не нужно
+ */
 function sayHello() {
   let userName = prompt('Введите ваше имя');
 
   if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
+  print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+  print('Некорректное имя');
   }
 }
+
+sayHello();

7e4a98ada680959bfeb40b39660d992f2cdbea38
Alexander Bezverchuk <albezver@gmail.com>
2020-09-15 15:41:36 +0300

    Функция возвращает строку с заглавной буквой

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,8 +4,6 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  if (str.length >= 1 || !str) {
-    str = str[0].toUpperCase() + str.slice(1);
-    return str;
-  }
+  if (!str) return str;
+  return str[0].toUpperCase() + str.slice(1);
 }

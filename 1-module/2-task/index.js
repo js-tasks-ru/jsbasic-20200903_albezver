d3276265323fa716f403827af72daf7b6aa8b57c
Alexander Bezverhuk <albezver@gmail.com>
2020-09-15 12:25:03 +0300

    Добавил проверку введенного имени на длину символов, наличие пробелов и отмену ввода

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,10 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name) {
-      return true;
-  } else {
+  if (!name || name.length < 4 || name.indexOf(' ') !== -1) {
       return false;
+  } else {
+      return true;
   }
 }
 

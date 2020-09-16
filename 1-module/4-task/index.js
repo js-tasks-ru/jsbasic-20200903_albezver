36424b84bad8021bc2ab2a07d755bc007f4f611b
Alexander Bezverchuk <albezver@gmail.com>
2020-09-16 18:16:31 +0300

    добавил проверку на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,23 @@
  * @param {string} str base
  * @returns {boolean}
  */
+/*
+ //Возвращает true для innocent rabbit
 function checkSpam(str) {
-  // ваш код...
+  if (str.includes('1xBet') || str.includes('XXX')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+*/
+
+function checkSpam(str) {
+  let strLower = str.toLowerCase();
+  if (strLower.includes('1xBet'.toLocaleLowerCase()) ||
+  str.includes('XXX'.toLowerCase())) {
+    return true;
+  } else {
+    return false;
+  }
 }
